409ce7dc88432a3f490e931a32c5aedd0b8cccb6
Martin Huynh <martinhu@uw.edu>
2021-10-03 17:41:38 -0700

    Fix memory leak and syntax.

--- factorial.cpp
+++ factorial.cpp
@@ -11,27 +11,27 @@
 using namespace std;
 
 int fact(int n) {
-  int Foo = 200;
+  // int foo = 200;
   if (n < 30) {
-    if (n <= 1)
+    if (n <= 1) {
       return 1;
+    }
     return n * fact(n - 1);
-  } else {
-    cout << "Too large: " << n << endl;
-    return -1;
   }
+  cout << "Too large: " << n << endl;
+  return -1;
 }
 
 int memoryLeakFunction() {
-  int x = 10;
-  int *Arr = new int[10];
+  // int x = 10;
+  int *arr = new int[10];
   for (int i = 0; i < 10; i++) {
-    Arr[i] = i;
+    arr[i] = i;
   }
   cout << "Hello World" << endl;
   cout << "Fact 5: " << fact(5) << endl;
   // memory leak if we do not delete it
-  // delete(Arr);
+  delete(arr);
   return 0;
 }
 
